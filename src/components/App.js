d667d11eaee0d94d062d9d749a74fdea43db02df
Pedro Nepomuceno <pedrohnlima@gmail.com>
2022-06-14 15:57:35 -0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import "../App.css";
 import Header from "./Header.js";
 import Main from "./Main.js";
 import Footer from "./Footer.js";
-import PopupWithForm from "./PopupWithForm.js";
+import PopupWithForm from "./opWithForm.js";
 import ImagePopup from "./ImagePopup.js";
 import React from "react";
 
